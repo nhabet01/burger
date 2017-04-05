a379bb919862d2249b5cf847ec96ca748c279869
Nahir Habet <Nahir Habet>
2017-04-05 11:49:05 -0400

    page icon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
+var path = require('path');
 var port = process.env.PORT || 7000;
 
 var app = express();
@@ -10,8 +10,8 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 //(Public folder hidden)
-app.use(express.static(process.cwd() + "/public"));
-// app.use("/static/", express.static(path.join(__dirname, "/public/")));
+// app.use(express.static(process.cwd() + "/public"));
+app.use("/static/", express.static(path.join(__dirname, "/public/")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
