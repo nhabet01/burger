6a4bd57b1cb05b83dc2748b56582f05e441b9e0d
Nahir Habet <Nahir Habet>
2017-04-06 14:59:46 -0400

    minor edits

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -22,6 +22,7 @@ router.get('/', function(req,res){
 router.post('/burgers/create', function(req,res){
     burger.insertOne(req.body.burger_name, function(result){
         //by redirecting to '/' we call the selectAll to render the burgers on the page
+        //alternatively could use res.render here as well to same or other page
         res.redirect('/');
     })
 })
--- models/burger.js
+++ models/burger.js
@@ -3,6 +3,8 @@ var orm = require ('../config/orm.js');
 var burger = {
 
     selectAll: function(cb){
+        //call selectAll from orm(already required), insert burgers as the table input,
+        //function(res) is the call back. All this gets sent to burgers_controllers file (routes)
         orm.selectAll('burgers',function(res){
             cb(res);
         })
